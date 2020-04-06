600d364d8de64052f14c8cd5c5e6779ea748509d
Salvador Sanchez <61708739+Sal183@users.noreply.github.com>
2020-04-05 22:39:23 -0500

    updated code for nightmare challenge

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,43 @@
 // Assignment code here
+function generatePassword() {
+  var CharSet = "";
 
+  var length = parseInt(window.prompt("Between 8 - 124, please choose the amount of characters you wish your password to have."));
+    if (length < 8 || length > 128) {
+      window.alert("Too Low or High value, please try again.");
+      return;
+    }
+  
+  var Lower = window.confirm("Include lowercase letters?");
+    if (Lower) {
+        CharSet += 'qwertyuiopasdfghjklzxcvbnm';
+    };
+
+  var Upper = window.confirm("Include uppercase letters?");
+    if (Upper) {
+        CharSet += 'QWERTYUIOPASDFGHJKLZXCVBNM';
+    };
+
+  var SpecialCharacters = window.confirm("Include symbols?");
+    if (SpecialCharacters) {
+        CharSet += '!@#$%^&*';
+    };
+
+  var Numbers = window.confirm("Include numbers?");
+    if (Numbers) {
+        CharSet += '0123456789';
+    };
+  
+  var Password = "";
+  for (let i =0; i < length; i++) {
+    Password += CharSet[Math.floor(Math.random() * CharSet.length)]
+  }
+  return Password;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
